130c5e0e59976ecdab0778dc0d6080b3f4a28f00
Jonah Rodriguez <jonahrodriguez95@gmail.com>
2020-11-02 16:13:02 -0700

    Added update annotations for TaskDao.

--- app/src/main/java/edu/cnm/deepdive/bard/model/dao/TaskDao.java
+++ app/src/main/java/edu/cnm/deepdive/bard/model/dao/TaskDao.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive.bard.model.dao;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Update;
 import edu.cnm.deepdive.bard.model.entity.Task;
 import io.reactivex.Single;
 import java.util.Collection;
@@ -19,6 +20,15 @@ public interface TaskDao {
 
   @Insert
   Single<List<Long>> insert(Collection<Task> task);
+
+  @Update
+  Single<Integer> update(Task task);
+
+  @Update
+  Single<Integer> update(Task... task);
+
+  @Update
+  Single<Integer> update(Collection<Task> task);
 
   @Delete
   Single<Integer> delete(Task task);
